2a83eb14186143cdf1b0332b83826f2d58230e86
KamalHajjar <kamalhajjar907@gmail.com>
2017-01-18 08:57:24 +0200

    toy problem 13

--- powerOfTwo/powerOfTwo.js
+++ powerOfTwo/powerOfTwo.js
@@ -5,5 +5,14 @@ Hint the answer is 1366.
 */
 
 var powerOfTwo = function(num){
-
+	if(num<=2**69){
+	var array = String(num).split("");
+	}else
+	array = String(num).split("e")[0].split(".")[1].split("")
+	array.push("1");
+	var sum =0; 
+		for (var i = 0; i < array.length; i++) {
+			sum +=  Number(array[i]);
+		}
+	return sum;
 }
