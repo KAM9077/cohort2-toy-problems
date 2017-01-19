346b02026d562adad206f9f9d57f0f7ada2a2af0
KamalHajjar <kamalhajjar907@gmail.com>
2017-01-19 09:17:09 +0200

    toy problem 14

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -18,7 +18,47 @@
 
 
 var LinkedList = function(){
-  //fill me in!
+  this.head = null;
+  this.tail = null;
 };
 
-//write methods here!
+LinkedList.prototype.addToTail = function(value) {
+	if (this.head === null){
+    this.tail = Node(value);
+    this.head = Node(value)
+	}
+var x = this.head
+	while(x.next !== null){
+		x = x.next
+	}
+	x.next = Node(value);
+	this.tail = value 
+  }
+
+LinkedList.prototype.removeHead = function(value) {
+	if(this.head!== null){
+		this.head = this.head.next;  
+	}
+}
+
+LinkedList.prototype.contains = function(value) {
+	var x = this.head;
+	while(x !== null){
+		if (x.value === value){
+		return true
+		}else{
+			x = x.next;
+		}
+	}
+	return false;
+}
+
+
+  var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+}
