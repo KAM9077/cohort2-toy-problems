036127854aba5c5de74522adfb33acfe6169447e
KAM9077 <kamalhajjar907@gmail.com>
2017-02-02 07:16:30 -0800

    toy problem 23

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -21,7 +21,29 @@
 /*
  * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.
 */
+//Without Extra credit
+// Array.prototype.isSubsetOf = function (arr) {
+	// for (var i = 0; i < this.length; i++) {
+	// 	if(arr.indexOf(JSON.stringify(this[i]))=== -1){
+	// 		return false;
+	// 	}
+	// }
+	// return true;
+// }
 
+
+//With Extra credit
 Array.prototype.isSubsetOf = function (arr) {
-
+	this.counter=0;
+	this.flage = true;
+	for (var j = 0; j < this.length; j++){ 
+	  this.counter=0;
+		for (var i = 0; i < arr.length; i++) {
+			if(JSON.stringify(arr[i])===(JSON.stringify(this[j]))){
+				this.counter++;
+			}
+		}
+		if(this.counter === 0){this.flage = false;}
+	}
+	return this.flage;
 }
